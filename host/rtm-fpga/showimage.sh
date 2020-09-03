cf35f7986035832d742bfe6cb47233310a3dac7b
clicia.s.pinto <clicia.s.pinto@cgpu2.ogun.local>
2020-09-03 17:49:53 -0300

    adding entire RTM source code

--- host/rtm-fpga/showimage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# $1 = nz
-# $2 = model folder
-ximage n1=$1 < output/dir.image perc=98 &
--- host/rtm-modmig/report.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aocl report ../../kernel/build/rtmforward2/rtmforward2.aocx profile.mon ../../kernel/build/rtmforward2/rtmforward2.source
