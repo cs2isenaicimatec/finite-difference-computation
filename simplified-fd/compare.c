5e668cb34b2e4e82a070aa14bec4e6d913a84619
Pedro Santana <pedro.gustavo.santana@outlook.com>
2020-09-28 08:44:39 -0300

    new compare code

--- simplified-fd/compare.c
+++ simplified-fd/compare.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-void compare(double *input1, double *input2, int len);
-void rmse(double *dif, int len);
-void stdev(double mean, double *dif, int len);
+void compare(float *input1, float *input2, int len);
+void rmse(float *dif, int len);
+void stdev(float mean, float *dif, int len);
 
 
 void main ()
 {
-    int mtxBufferLength = (315 + 2 * 50)*(195 + 2 * 50)*sizeof(double);
-    double output_cuda[mtxBufferLength], output_dpc[mtxBufferLength];
+    int mtxBufferLength = (315 + 2 * 50)*(195 + 2 * 50)*sizeof(float);
+    float output_cuda[mtxBufferLength], output_dpc[mtxBufferLength];
 
     FILE *foutput_cuda, *foutput_dpc;
     foutput_cuda = fopen("output_cuda.bin", "rb");
@@ -25,50 +25,62 @@ void main ()
     compare(output_cuda, output_dpc, mtxBufferLength);
 }
 
-void compare(double *input1, double *input2, int len)
+void compare(float *input1, float *input2, int len)
 {
     int i;
-    double dif[len], mae, sum, acc;
-    int count = 0;
+    float dif[len], mae, sum, acc;
+    int count = 0, length = 0;
     for (i = 0; i < len; i++)
     {
+        if (fabsf(input1[i]) < 1000000 && fabsf(input2[i]) < 1000000)
+        {
             dif[i] = fabsf(input1[i] - input2[i]);
             sum += dif[i];
             if (input1[i] == input2[i])
             {
-                    count++;
+                count++;
             }
-	    else
-	    {
-		    printf("%.35f\t-\t%.35f\n", input1[i],input2[i]);
-	    }
+            length++;
+        }
+        else
+        {
+            dif[i] = NULL;
+        }
     }
-    mae = sum / (double)len;
-    acc = (double)count/(double)len*100;
+    mae = sum / (float)length;
+    acc = (float)count/(float)len*100;
     printf("Accuracy: %.5f%\n", acc);
     rmse(dif,len);
     printf("MAE: %.15f\n",mae);
     stdev(mae, dif, len);
 }
 
-void rmse(double *dif, int len)
+void rmse(float *dif, int len)
 {
-    int i;
-    double sum = 0.0;
+    int i, length = 0;
+    float sum = 0.0;
     for (i = 0; i < len; i++)
     {
+        if (dif[i] != NULL)
+        {
             sum += pow(dif[i], 2);
+            length++;
+        }
     }
-    printf("RMSE: %.15f\n", sqrt(sum/(double)len));
+    printf("RMSE: %.15f\n", sqrt(sum/(float)length));
 }
 
-void stdev(double mean, double *dif, int len)
+void stdev(float mean, float *dif, int len)
 {
-    int i;
-    double p = 0.0, sigma;
+    int i, length = 0;
+    float p = 0.0, sigma;
     for(i = 0; i < len; i++){
+        if (dif[i] != NULL)
+        {    
             p = p + pow(dif[i] - mean, 2);
+            length++;
+        }
     }
-    sigma = sqrt(p/((double)len-1));
+    sigma = sqrt(p/((float)length-1));
     printf("Standard deviation: %.15f\n", sigma);
 }
