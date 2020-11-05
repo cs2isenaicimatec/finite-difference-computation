32181034d784701efc86f74b4cd35f3e644481b6
pedro.gustavo <pedro.gustavo@login0.ogun.local>
2020-11-05 12:09:54 -0300

    compare update

--- simplified-fd/compare.c
+++ simplified-fd/compare.c
@@ -5,11 +5,12 @@
 #include "functions.h"
 
 void compare(float *input1, float *input2, int len);
+void compare2D(float **input1, float **input2, int x, int y);
 void rmse(float *dif, int len);
 void stdev(float mean, float *dif, int len);
 FILE* read_file(char *path);
 
-void compare2D(int x, int z)
+void compare2alloc(int x, int z)
 {
     int mtxBufferLength = x*z*sizeof(float);
     float **output_1;
@@ -20,8 +21,10 @@ void compare2D(int x, int z)
     memset(*output_2, 0, mtxBufferLength);
 
     FILE *foutput_1, *foutput_2;
-    foutput_1 = read_file("../host/rtm-cuda/output/dir.image");
+    foutput_1 = read_file("./dir.image");
     foutput_2 = read_file("./complete-code/output/dir.image");
+    if (foutput_1 == NULL || foutput_2 == NULL)
+        return;
     printf("Comparação de binários 2D:\n");
     printf("Lendo arquivos...\n");
     fread(*output_1, sizeof(float), x*z, foutput_1);
@@ -30,7 +33,7 @@ void compare2D(int x, int z)
     fclose(foutput_2);
 
     printf("Comparando...\n");
-    compare(*output_1, *output_2, x*z);
+    compare2D(output_1, output_2, x, z);
     free2float(output_1);
     free2float(output_2);
 }
@@ -48,6 +51,8 @@ void compare1alloc(int len)
     FILE *foutput_1, *foutput_2;
     foutput_1 = read_file("../host/rtm-cuda/file-teste");
     foutput_2 = read_file("./complete-code/file-teste");
+    if (foutput_1 == NULL || foutput_2 == NULL)
+        return;
     printf("Comparação de binários 1D:\n");
     printf("Lendo arquivos...\n");
     fread(output_1, sizeof(float), len, foutput_1);
@@ -64,9 +69,36 @@ void compare1alloc(int len)
 void main ()
 {
     
-    // compare3Dread(6,315,1700);
+    compare2alloc(195,315);
     printf("\n");
-    compare1alloc(315*195);
+    // compare1alloc(315*195);
+}
+
+void compare2D(float **input1, float **input2, int x, int y)
+{
+    int ix, iy;
+    float dif[x][y], mae, sum = 0.0, acc;
+    int count = 0, length = 0;
+    for (ix = 0; ix < x; ix++)
+    {
+        for (iy = 0; iy < y; iy++)
+        {
+            dif[ix][iy] = fabsf(input1[ix][iy] - input2[ix][iy]);
+            sum += dif[ix][iy];
+            if (dif[ix][iy] == 0.0)
+            {
+                count++;
+            }
+            
+            length++;
+            }
+    }
+    mae = sum / (float)length;
+    acc = (float)count/(float)x*y*100;
+    printf("Accuracy: %.2f%\n", acc);
+    // rmse(dif,len);
+    printf("MAE: %e\n",mae);
+    // stdev(mae, dif, len);
 }
 
 void compare(float *input1, float *input2, int len)
@@ -81,10 +113,6 @@ void compare(float *input1, float *input2, int len)
         if (dif[i] == 0.0)
         {
             count++;
-        }
-        else
-        {
-            printf("%.30f\n", dif[i]);
         }
         
         length++;
@@ -127,7 +155,7 @@ FILE* read_file(char *path)
     file = fopen(path, "r");
     if(file == NULL)
     {
-        perror("File 1: ");
+        perror(path);
         return;
     }
     return file;
